bd1320fb11c2efe672ceded3b6cb3e4912ff5565
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-04-09 16:14:51 -0400

    imports added in test form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -40,7 +40,7 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
+          <label htmlFor="email" id="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
           <input name="email" ref={register({ required: true })} />
@@ -50,7 +50,7 @@ const ContactForm = () => {
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea name="message" id="textarea" ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+import ContactForm from './ContactForm';
+
+test('')
